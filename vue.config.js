c94fb96646f4d0606222a3685bc912ff60c179dc
Константин Тимофеев <kt@webink.site>
2023-10-16 14:51:24 +0300

    fix all

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '',
+  publicPath: '/fireex/',
   transpileDependencies: true,
   pwa: {
     appleMobileWebAppCapable: 'yes',
